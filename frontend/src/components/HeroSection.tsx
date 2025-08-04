737a32fbf149c1d51a3cc1d3e54d82456253c6f3
salpuree <144355733+salpuree@users.noreply.github.com>
2025-08-03 23:58:33 -0700

    Updated HeroSection.tsx

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -187,7 +187,7 @@ const HeroSection = () => {
               Your Concierge Back Office for
             </span>
             {/* EDIT: Applied gradient text style to the typewriter effect */}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-650 to-slate-300">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-650 to-purple-500 to-slate-300">
               <TypewriterEffect words={rotatingWords} />
             </span>
           </h1>
