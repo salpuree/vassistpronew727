4508d0fe6a79cc0e23f33f1059f9051ee19330d5
salpuree <144355733+salpuree@users.noreply.github.com>
2025-08-07 12:58:46 -0700

    Updated vite.config.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// Required for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   build: {
     outDir: 'build'
   },
@@ -15,11 +20,6 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
     allowedHosts: true
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
   }
 })
+
