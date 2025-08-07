8b538f52ccab12e19813af8e75cf8b4ada508ae3
salpuree <144355733+salpuree@users.noreply.github.com>
2025-08-07 12:51:45 -0700

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'build'
   },
   server: {
     port: 3000,
-    host: '0.0.0.0', // Bind to all interfaces (needed for Netlify or external access)
+    host: '0.0.0.0'
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
